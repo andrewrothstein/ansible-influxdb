c99cc1df3daf601da864b87a66813356878e8a5b
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-21 22:12:52 -0500

    dl-checksums.sh cleanup

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
-
 DIR=~/Downloads
-MIRROR=https://dl.influxdata.com/influxdb/releases
+APP=influxdb
+MIRROR=https://dl.influxdata.com/${APP}/releases
 
 dl() {
-    ver=$1
-    os=$2
-    arch=$3
-    archive_type=$4
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
     local platform="${os}_${arch}"
-    local file=influxdb-${ver}_${platform}.${archive_type}
+    local file=${APP}-${ver}_${platform}.${archive_type}
     local url=$MIRROR/$file
     local lfile=$DIR/$file
     printf "    # %s\n" $url
@@ -17,15 +17,16 @@ dl() {
     then
         wget -q -O $lfile $url
     fi
+    printf "    # %s\n" $url
     printf "    '%s': sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
 }
 
 dl_ver() {
     local ver=$1
     printf "  '%s':\n" $ver
-    dl $ver linux amd64 tar.gz
-    dl $ver linux i386 tar.gz
-    dl $ver linux armhf tar.gz
+    dl $ver linux amd64
+    dl $ver linux i386
+    dl $ver linux armhf
     dl $ver windows amd64 zip
 }
 
